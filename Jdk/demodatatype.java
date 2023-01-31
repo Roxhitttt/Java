5c9b8d217fe2b61aa8e35400577cd862e1d3f3a4
Rohit Gosavi <62470301+Roxhitttt@users.noreply.github.com>
2023-01-31 20:49:54 +0530

    Add files via upload

---
+++ Jdk/demodatatype.java
@@ -0,0 +1,15 @@
+class demodatatype
+{
+    public static void main(String args[])
+    {
+       int a;
+       a=12345;
+       System.out.println("a="+a);
+       float b;
+       b=3456.678f;
+       System.out.println("b="+b);
+       byte l=127;
+       System.out.println("l="+l);
+    
+    }
+}
