599993f3164887ff2fe75fea3f040703513278b5
Rohit Gosavi <rohit02032001@gmail.com>
2023-02-02 23:46:41 +0530

    java

---
+++ Jdk/Demorel.java
@@ -0,0 +1,23 @@
+class Demorel
+{
+    public static void main(String args[])
+    {
+              int a=20;
+              int b=30;
+              boolean c;
+              c=a>b;
+              System.out.println(a+">"+b+" is "+c);
+              c=a<b;
+              System.out.println(a+"<"+b+" is "+c);
+              c=a>=b;
+              System.out.println(a+">="+b+" is "+c);
+              c=a<=b;
+              System.out.println(a+"<="+b+" is "+c);
+              c=a==b;
+              System.out.println(a+"=="+b+" is "+c);
+              c=a!=b;
+              System.out.println(a+"!="+b+" is "+c);
+
+
+    }
+}
---
+++ Jdk/demoArithmatic.java
@@ -0,0 +1,24 @@
+class demoArithmatic
+{
+    public static void main(String args[])
+    {
+    int a=10;
+    int b=20;
+    int c;
+    c=a+b;
+    System.out.println(c=a+b);
+    System.out.println("addtion of"+a+"and"+b+"is"+c);
+    c=a-b;
+    System.out.println(c=a-b);
+    System.out.println("Subtraction of "+a+"and"+b+"is"+c);
+    c=a*b;
+    System.out.println(c=a*b);
+    System.out.println("Multiplication of "+a+"and"+b+"is"+c);
+    c=a/b;
+    System.out.println(c=a/b);
+    System.out.println("Division of "+a+"and"+b+"is"+c);
+    c=a%b;
+    System.out.println(c=a%b);
+    System.out.println("Modulus of "+a+"and"+b+"is"+c);
+    }
+}
