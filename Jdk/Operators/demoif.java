2cf2439555cbbca31c857bca4c67fef9dfea7760
Roxhitttt <rohit02032001@gmail.com>
2023-02-14 19:02:31 +0530

    java

---
+++ Jdk/Operators/demoif.java
@@ -0,0 +1,29 @@
+class demoif
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=10;
+        b=20;
+        if(a>b)
+        {
+            System.out.println("Grater is"+a);
+        }
+        else
+        {
+            System.out.println("Grater is"+b);
+        }
+
+        a=599;
+        b=88;
+        if(a>b)
+        {
+            System.out.println("Grater is"+a);
+        }
+        if(a<b)
+        {
+            System.out.println("Grater is"+b);
+        }
+        
+    }
+}
