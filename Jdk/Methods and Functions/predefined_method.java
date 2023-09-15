1e1b5ca1ac3d11a9507afd3e86c18ed0403a3584
Rohit Bava <rohit02032001@gmail.com>
2023-09-15 11:22:19 +0530

    dsf

---
+++ Jdk/Methods and Functions/predefined_method.java
@@ -0,0 +1,6 @@
+public class predefined_method {
+    public static void main(String args[]){
+        // using the max() method of Math class 
+        System.out.println("The maximum number is : " + Math.max(9,5));
+    }
+}
