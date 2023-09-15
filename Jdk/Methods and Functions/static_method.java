75a0385d5503d983e5388271980588f83302eb64
Rohit Bava <rohit02032001@gmail.com>
2023-09-15 11:53:06 +0530

    cc

---
+++ Jdk/Methods and Functions/static_method.java
@@ -0,0 +1,8 @@
+public class static_method {
+    static void show(){
+        System.out.println("It is an example of static method");
+    }
+    public static void main(String args[]){
+        show();
+    }
+}
